1a8ffaf7bd5007ca2dc9276b85ea7d9b5ea6ab65
chiangd233 <chiangd233@gmail.com>
2021-12-17 03:12:15 -0500

    adds back timer and lives left

--- script.js
+++ script.js
@@ -1,95 +1,6 @@
 
-// // Timer function for Countdown
-// const startingMinutes = 5;
-// let timeLeft = startingMinutes * 60;
-// const timerEl = document.getElementById('timer');
-// setInterval (updateTimer, 1000);
 
-// function updateTimer () {
-//     if (timeLeft <= 0) {
-//         timerEl.innerHTML = `0:00`
-//         return
-//         // if (lives < 0) {
-//         //     return 'you win'
-//         // } else {
-//         //     return 'you lose'
-//         // }
-//     }
-//     const minutes = Math.floor(timeLeft / 60);
-//     let seconds = timeLeft % 60;
-//     seconds = seconds < 10 ? '0' + seconds : seconds 
-//     timerEl.innerHTML = `${minutes}:${seconds}`;
-//     timeLeft--;
-    
-// }
 
-// // lives and function for live decrease
-// let lives = 5;
-// const livesEl = document.getElementById('lives');
-// const wallEl =  document.getElementById('wall');
-// livesEl.innerHTML = `Lives Left ${lives}`;
-
-// wallEl.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     decreaseLives();
-// })
-
-// function decreaseLives () {
-//     if (lives <= 0) {
-//         livesEl.innerHTML = `Lives Left 0`
-//         return
-//     }
-//     lives = lives - 1;
-//     livesEl.innerHTML = `Lives Left ${lives}`;
-// }
-
-// // Creation of enemy characters
-// // class Enemy {
-// //     constructor(verticalPosition) {
-// //         this.
-// //     }
-// // }
-// SPAWN_RATE = 0.2
-// function check_spawn(spawn_rate) {
-//     return (Math.random() <= spawn_rate);
-// }
-
-// let lanesEl = document.querySelectorAll('.lane');
-// setInterval (spawnEnemies, 1000);
-
-// function spawnEnemies() {
-//     lanesEl.forEach(lane => {
-//         if (check_spawn(SPAWN_RATE)) {
-//             let divEl = document.createElement("div");
-//             divEl.classList.add('enemy');
-//             lane.appendChild(divEl);
-//         }
-//     });
-// }
-
-//  let position = 0 
-
-//  function moveEnemies() {
-
-//  }
-//  setInterval(function () {
-//     // let enemiesEl = document.querySelectorAll('.enemy')
-
-//     // enemiesEl.forEach(enemy => {
-//     //     position += 5;
-//     //     console.log(enemy.style);
-//     //     console.log(enemy.style.right);
-//     //     enemy.style.position = 'relative';
-
-//     //     enemy.style.right = position + "px";
-//     // })
-//     let offset = $(".enemy").offset()
-//     offset.right += 10 + "px";
-//     $(".enemy").offset(offset)
-    
-//  }, 1000)
-
-//  // onclick function to remove enemies
 // Global Variables
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext('2d');
@@ -102,7 +13,51 @@ const enemies = [];
 let enemyPosition = [];
 let frame = 0;
 
+// Timer function for game
+const startingMinutes = 0.1;
+let timeLeft = startingMinutes * 60;
+const timerEl = document.getElementById('timer');
+setInterval (updateTimer, 1000);
+
+function updateTimer () {
+    if (timeLeft <= 0) {
+        timerEl.innerHTML = `0:00`
+        return
+        // if (lives < 0) {
+        //     return 'you win'
+        // } else {
+        //     return 'you lose'
+        // }
+    }
+    const minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds 
+    timerEl.innerHTML = `${minutes}:${seconds}`;
+    timeLeft--;
+
+}
+
+// lives and function for live decrease
+let lives = 5;
+const livesEl = document.getElementById('lives');
+livesEl.innerHTML = `Lives Left ${lives}`;
+
+// wallEl.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     decreaseLives();
+// })
+
+function decreaseLives () {
+    if (lives <= 0) {
+        livesEl.innerHTML = `Lives Left 0`
+        return
+    }
+    lives = lives - 1;
+    livesEl.innerHTML = `Lives Left ${lives}`;
+}
+
 // Creation of Gameboard
+
 const castleWall = {
     width: cellSize,
     height: canvas.height,
@@ -174,10 +129,10 @@ function enemyMove () {
     for (let i = 0; i < enemies.length; i++) {
         enemies[i].move()
         enemies[i].draw()
-        // if (enemies[i].x < cellSize) {
-        //     ctx.fillStyle = "blue";
-        //     ctx.fillRect(0, 0, enemies[i].width, canvas.height)
-        // }
+        if (enemies[i].x < cellSize / 2) {
+            enemies.splice(i,1)
+            decreaseLives ()
+        }
     }
     if (frame % 25 === 0){
         if(Math.random() <= spawnRate) {
